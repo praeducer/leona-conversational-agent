603c4a98a809d0b68e51f561de975cbe188b7142
Paul Prae <paul.prae@outlook.com>
2017-08-06 16:56:48 -0400

    inital commit. added in boilerplate from previous amazon lex project. includes lambda function with lots of helper functions and a basic intent handler. also has the grunt auto deploy setup. plus readme and iam policies.

---
+++ lambda/leona-code-hook/Gruntfile.js
@@ -0,0 +1,39 @@
+// https://github.com/Tim-B/grunt-aws-lambda
+//    lambda_invoke - Wrapper to run and test lambda functions locally and view output.
+//    lambda_package - Packages function along with any npm dependencies in a zip format suitable for lambda.
+//    lambda_deploy - Uploads the zip package to lambda.
+
+var grunt = require('grunt');
+grunt.loadNpmTasks('grunt-aws-lambda');
+
+grunt.initConfig({
+   lambda_invoke: {
+      default: {
+         options: {
+             //Name of the handler function to invoke.
+             //handler: 'LeonaCodeHook.handler',
+             //Name of your script file which contains your handler function.
+            file_name: 'LeonaCodeHook.js'
+         }
+      }
+   },
+   lambda_deploy: {
+      default: {
+          // TODO: The ARN of your target Lambda function.
+         //arn: 'arn:aws:lambda:us-east-1:800754320539:function:AIVACodeHook',
+         options: {
+             // Sets the handler for your lambda function. If left null, the current setting will remain unchanged.
+             //handler: 'LeonaCodeHook.handler'
+             // If you wish to use a specific AWS credentials profile you can specify it here, otherwise it will use the environment default. You can also specify it with the environment variable AWS_PROFILE
+             profile: 'praeducer'
+             //If you wish to use hardcoded AWS credentials saved in a JSON file, put the path to the JSON here. The JSON must conform to the AWS format.
+         }
+      }
+   },
+   lambda_package: {
+      default: {
+   }
+   }
+});
+
+grunt.registerTask('deploy', ['lambda_package', 'lambda_deploy'])
---
+++ lambda/leona-code-hook/LeonaCodeHook.js
@@ -0,0 +1,227 @@
+'use strict';
+
+ /**
+  * To deploy, run: grunt deploy --verbose
+  */
+
+
+ // --------------- Helpers to build responses which match the structure of the necessary dialog actions -----------------------
+function elicitIntent(sessionAttributes, messageContent) {
+    return {
+        sessionAttributes,
+        dialogAction: {
+            type: 'ElicitIntent',
+            message: {
+              contentType: 'PlainText',
+              content: messageContent
+            }
+        }
+    };
+}
+
+// NOTE: Response cards are not currently supported with ElicitIntent dialog action
+function elicitIntentWithCard(sessionAttributes, messageContent, responseCard) {
+    return {
+        sessionAttributes,
+        dialogAction: {
+            type: 'ElicitIntent',
+            message: {
+              contentType: 'PlainText',
+              content: messageContent
+            },
+            responseCard : responseCard
+        }
+    };
+}
+// TODO: Experiment with response cards
+// NOTE: Response cards are not currently supported with ElicitIntent dialog action
+function elicitIntentWithCardDetails(sessionAttributes, messageContent) {
+    return {
+        sessionAttributes,
+        dialogAction: {
+            type: 'ElicitIntent',
+            message: {
+              contentType: 'PlainText',
+              content: messageContent
+            },
+            responseCard: {
+                version: 1,
+                contentType: "application/vnd.amazonaws.card.generic",
+                genericAttachments: [
+                {
+                    title: "What Flavor?",
+                    subTitle: "What flavor do you want?",
+                    // imageUrl: "Link to image",
+                    // attachmentLinkUrl: "Link to attachment",
+                    buttons: [
+                    {
+                        text: "Lemon",
+                        value: "lemon"
+                    },
+                    {
+                        text: "Raspberry",
+                        value: "raspberry"
+                    },
+                    {
+                        text: "Plain",
+                        value: "plain"
+                    }
+                    ]
+                }
+                ]
+            }
+        }
+    };
+}
+
+// e.g. callback(elicitSlot(intentRequest.sessionAttributes, intentRequest.currentIntent.name, slots, validationResult.violatedSlot, validationResult.message));
+function elicitSlot(sessionAttributes, intentName, slots, slotToElicit, message) {
+    return {
+        sessionAttributes,
+        dialogAction: {
+            type: 'ElicitSlot',
+            intentName,
+            slots,
+            slotToElicit,
+            message,
+        },
+    };
+}
+
+function elicitSlotWithCard(sessionAttributes, intentName, slots, slotToElicit, message, responseCard) {
+    return {
+        sessionAttributes,
+        dialogAction: {
+            type: 'ElicitSlot',
+            intentName,
+            slots,
+            slotToElicit,
+            message,
+            responseCard,
+        },
+    };
+}
+
+function confirmIntent(sessionAttributes, intentName, slots, message, responseCard) {
+    return {
+        sessionAttributes,
+        dialogAction: {
+            type: 'ConfirmIntent',
+            intentName,
+            slots,
+            message,
+            responseCard,
+        },
+    };
+}
+
+function close(sessionAttributes, fulfillmentState, message) {
+    return {
+        sessionAttributes,
+        dialogAction: {
+            type: 'Close',
+            fulfillmentState,
+            message,
+        },
+    };
+}
+
+function delegate(sessionAttributes, slots) {
+    return {
+        sessionAttributes,
+        dialogAction: {
+            type: 'Delegate',
+            slots,
+        },
+    };
+}
+
+// ---------------- Helper Functions --------------------------------------------------
+function buildValidationResult(isValid, violatedSlot, messageContent) {
+    if (messageContent == null) {
+        return {
+            isValid,
+            violatedSlot,
+        };
+    }
+    return {
+        isValid,
+        violatedSlot,
+        message: { contentType: 'PlainText', content: messageContent },
+    };
+}
+
+// Build a responseCard with a title, subtitle, and an optional set of options which should be displayed as buttons.
+function buildResponseCard(title, subTitle, options) {
+    let buttons = null;
+    if (options != null) {
+        buttons = [];
+        for (let i = 0; i < Math.min(5, options.length); i++) {
+            buttons.push(options[i]);
+        }
+    }
+    return {
+        contentType: 'application/vnd.amazonaws.card.generic',
+        version: 1,
+        genericAttachments: [{
+            title,
+            subTitle,
+            buttons,
+        }],
+    };
+}
+
+ // --------------- Functions that control the bot's behavior -----------------------
+
+/**
+ * Performs dialog management and fulfillment.
+ */
+function hello(intentRequest, callback) {
+    // Display the content, and rely on the goodbye message of the bot to define the message to the end user.
+    callback(elicitIntent(intentRequest.sessionAttributes,
+        `Hello!`
+    ));
+}
+
+ // --------------- Intents -----------------------
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function dispatch(intentRequest, callback) {
+    console.log(`dispatch userId=${intentRequest.userId}, intentName=${intentRequest.currentIntent.name}`);
+
+    const intentName = intentRequest.currentIntent.name;
+
+    // Dispatch to your skill's intent handlers
+    if (intentName === 'Hello') {
+        return hello(intentRequest, callback);
+    }
+    throw new Error(`Intent with name ${intentName} not supported`);
+}
+
+// --------------- Main handler -----------------------
+
+// Route the incoming request based on intent.
+// The JSON body of the request is provided in the event slot.
+exports.handler = (event, context, callback) => {
+    try {
+        // By default, treat the user request as coming from the America/New_York time zone.
+        process.env.TZ = 'America/New_York';
+        console.log(`event.bot.name=${event.bot.name}`);
+
+        /**
+         * Uncomment this if statement and populate with your Lex bot name and / or version as
+         * a sanity check to prevent invoking this Lambda function from an undesired Lex bot or
+         * bot version.
+         */
+        /*
+        if (event.bot.name !== 'OrderFlowers') {
+             callback('Invalid Bot Name');
+        }
+        */
+        dispatch(event, (response) => callback(null, response));
+    } catch (err) {
+        callback(err);
+    }
+};
